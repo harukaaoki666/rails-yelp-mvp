08b5ae5e818cb24ec602ae9ba46d8dce4a82f656
Haruka Aoki <solchan666@gmail.com>
2018-06-06 12:12:33 +0200

    Add restaurant model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants
 end
+
+
+#rails g scaffold Restaurant name address category:text rating:integer
